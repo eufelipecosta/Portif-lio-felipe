4f09b5323d58ca32e4d3d370d2b0b173a2b3021c
Felipe Costa <eu.felipecosta@hotmail.com>
2023-05-31 18:56:40 -0300

    1.11

--- JS/script.js
+++ JS/script.js
@@ -51,8 +51,8 @@ ScrollReveal({
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'rigth' });
+ScrollReveal().reveal('.home-content h1, .about-img, .about-content', { origin: 'left' });
+
 
 
 
